78e844dffeabe18ad0cd7e0571ed9c5fbd4a7b49
Niko Nastonen <niko.nastonen@icloud.com>
2022-08-03 13:56:11 +0300

    rename

--- book/echo_module/echo.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define ECHO_CLEAR_BUFFER       _IO('E', 1)
-#define ECHO_SET_BUFFER_SIZE    _IOW('E', 2, int)
--- book/race_module/race_ioctl.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define RACE_NAME		"race"
-
-#define RACE_IOC_ATTACH		_IOR('R', 0, int)
-#define RACE_IOC_DETACH		_IOW('R', 1, int)
-#define RACE_IOC_QUERY		_IOW('R', 2, int)
-#define RACE_IOC_LIST		_IO('R', 3)
-
